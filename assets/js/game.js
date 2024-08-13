e4c476a762749c1f9dcf058790dd97e6c9266e41
Shaun Anderton <shaunanderton1@gmail.com>
2024-08-13 19:01:44 +0000

    added first attempt at populating select fields

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,9 @@
 
 // then finally a variety of helper functions to  do various things throughout the game
 
+// HTML ELEMENTS
+const parent = document.getElementById("difficulty");
+
 //------- VARIABLES -------//
 
 // this object will store our users selections for us to act on later
@@ -45,7 +48,7 @@ const category_options = [
 ];
 
 // difficulty
-export const difficulty_options = [
+const difficulty_options = [
   { value: "easy", label: "Easy" },
   { value: "medium", label: "Medium" },
   { value: "hard", label: "Hard" },
@@ -66,6 +69,30 @@ let numberOfQuestions;
 // and select boxes, this will allow us to get the data back from user input and feed it to the
 // options object
 
-// here are our categories
+// our first major function will be creating the game categories and difficulties, to do this we will
+// create a loop that goes over the above objects and inserts them into the html
 
+// this will generate the markup for our options
+function generateMarkup(arr, category) {
+  return `
+      <div class="option">
+        <input 
+         type ="radio" 
+         class="radio"
+         id="${arr.value}"
+         name="${category}"
+        />
+        <label for="${arr.value}">${arr.label}</label>
+      </div>
+   `;
+}
+
+// now looping function to make our options work
+function createCategories(arr) {
+  for (let i = 0; i < arr.length; i++)
+    parent.appendChild(generateMarkup(arr, "category"));
+}
+
+createCategories(category_options);
+console.log(createCategories);
 // now we make a functions
