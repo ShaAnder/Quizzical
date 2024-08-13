837bb3177a5368a5c2c1f93e2e554d0f6b8c2bbd
Shaun Anderton <shaunanderton1@gmail.com>
2024-08-13 14:23:42 +0000

    added modal window + styling added first pass of modal js to get window to work

---
+++ assets/js/modal.js
@@ -0,0 +1,20 @@
+// JS For the modal window, wanted to implement first to ensure it was functioning correctly
+
+// as stated in css=, created using this tutorial ->  https://www.youtube.com/watch?v=XH5OW46yO8I
+
+// Modal variables to handle modal opening
+const openModal = document.getElementById("open-modal");
+const closeModal = document.getElementById("close-modal");
+const modalContainer = document.getElementById("modal-container");
+
+// Open Modal
+openModal.addEventListener("click", () => {
+  modalContainer.classList.add("open");
+});
+
+// Close Modal
+closeModal.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
+
+// potential bug? js validator shows warnings on arrow functions, this was discussed with mentor previously that it doesn't understand arrow functions properly. will look to fix in a later update
